b60903a88c501c99e37adf4313bcadad6a16be55
M-Rodrigue <morel.rodrigue@protonmail.com>
2021-12-13 19:37:27 +0100

    TP

---
+++ cours.js
@@ -0,0 +1,21 @@
+/* Exercice 1 */
+// Avant permutation
+//let a = prompt("Modifier A");
+//let b = prompt("Modifier B");
+//alert(`Avant permutation :\na=${a} et b=${b}`);
+
+// Après permutation
+//let c = a;
+//a = b;
+//b = c;
+//alert(`Après permutation :\na=${a} et b=${b} puis c=${c}`);
+
+/* Exercice 2 */
+//let tweet = prompt("Composer votre tweet :")
+//let tweetCount = tweet.length;
+//alert(`Vous avez écris ${tweetCount} caractères, il vous reste ${140 - tweetCount} caractères`);
+
+/* Exercice 3 */
+let tweet = prompt("Composer votre tweet :")
+let tweetCount = tweet.length;
+alert(`Vous avez écris ${tweetCount} caractères, il vous reste ${140 - tweetCount} caractères`);
